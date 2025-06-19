2ad483b14aa7a392bb35516f3971e79b9af95ab2
David Santos <david.tecds@gmail.com>
2025-06-19 10:03:19 -0300

    Update: Lucide icons adicionado

--- Port/src/Pages/Home.tsx
+++ Port/src/Pages/Home.tsx
@@ -1,22 +1,28 @@
 import '../App.css'
 import Perfil from '../assets/Perfil.png'
+import { instagram } from "lucide-react"
 
 function Home() {
-
 return (
 
 <div className="flex bg-white w-screen h-screen">
-  <div className="w-1/2 flex items-center text-gray-900 text-9xl font-bold justify-center">
-  <div className="flex flex-col">
-    <span className='italic'>Hello</span>
-    <span className='ml-10 italic'>World</span>
+  <div className="w-1/2 flex items-center  justify-center">
+  <div className="flex flex-col text-gray-900 text-9xl font-bold">
+    <span className='italic text-shadow-lg'>Hello</span>
+    <span className='ml-10 italic text-shadow-lg'>World</span>
     <span className='text-gray-700  text-5xl ml-17'> I'm a Dev</span>
+      <button className='w-40 h-16 !text-2xl text-blue-700 font-bold !bg-gray-200 mt-20 shadow-xl/20'> Contact </button>
   </div>
-  <span className="ml-3 text-[250px] transform origin-bottom scale-y-150 scale-x-150 text-blue-700">!</span>
+
+  <span className="ml-3 mt-25 text-[250px] font-bold transform origin-bottom scale-y-160 scale-x-150 text-blue-700 text-shadow-lg">!</span>
 </div>
-  <div className="w-1/2 flex flex-col items-center justify-center">
+  <div className="w-1/2 flex flex-col relative items-center justify-center">
     <img src={Perfil} alt="Perfil" className='bg-blue-700  rounded-full max-w-150 shadow-xl/30'/>
+    <span className="text-gray-900 absolute mt-150 text-8xl font-bold ">Name</span>
   </div>
+
+  <instagram/>
+
 </div>
 
 )
